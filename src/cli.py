5e6b1b1847205afa867f43f130fee41f8917ef4c
Jayanta <jayanta.dn@gmail.com>
2025-06-11 20:01:02 +0530

    start and stop timer

--- src/cli.py
+++ src/cli.py
@@ -1,10 +1,14 @@
 import sys
 import dataset
 import datetime
+import os
 
 # database connection
 db = dataset.connect('sqlite:///project-timer.db')
 table = db["time_records"]
+
+# globals
+projects = []
 
 
 def create_task():
@@ -33,21 +37,29 @@ def process_user_input():
         daily_report()
     elif choice == "x":
         exit()
+    elif int(choice) > 0 and int(choice) <= len(projects):
+        show_tasks(int(choice))
     else:
         print("Invalid choice. Please try again.")
         process_user_input()
 
 
+def refresh():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    show_projects_status()
+    show_default_options()
+    process_user_input()
+
+
 def show_default_options():
     print("0. New task")
     print("\n[s] Stop timer")
-    print("[d] Daily report")
-    print("[t] Task report")
     print("[x] Exit")
 
 
-def show_project_list():
+def show_projects_status():
     global table
+    global projects
 
     # Project list
     projects = [{"project": row['project']}
@@ -77,12 +89,35 @@ def show_project_list():
     # display
     print("\n")
     for idx, proj in enumerate(projects, 1):
-        duration = proj.get('duration', datetime.timedelta())
+        if 'duration' in proj:
+            duration = proj.get('duration', datetime.timedelta())
+        else:
+            duration = 0
         hours_decimal = duration.total_seconds() / 3600
         running = ""
-        if proj['timer_running']:
+        if 'timer_running' in proj and proj['timer_running']:
             running = "*"
         print(f"{idx}. {proj['project']} - {hours_decimal:.2f}{running} hrs")
+
+
+def show_tasks(project_id):
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    project_name = projects[project_id - 1]['project']
+    tasks = list({row['task'] for row in table.find(project=project_name)})
+    if not tasks:
+        print(f"No tasks found for project '{project_name}'.")
+    else:
+        print(f"Tasks for project '{project_name}':")
+        for idx, task in enumerate(tasks, 1):
+            print(f"{idx}. {task}")
+
+    task_id = int(input("\n Select task to start: "))
+    if task_id > 0 and task_id <= len(tasks):
+        start_timer(projects[project_id - 1]['project'], tasks[task_id - 1])
+    else:
+        print("Invalid input")
+        show_tasks(project_id)
 
 
 def start_timer(project, task):
@@ -93,16 +128,24 @@ def start_timer(project, task):
         "start_time": datetime.datetime.now().isoformat(),
         "end_time": None
     })
+    refresh()
 
 
 def stop_timer():
-    pass
+    running_records = list(table.find(end_time=None))
+    if not running_records:
+        return
+    for record in running_records:
+        table.update(
+            dict(id=record['id'],
+                 end_time=datetime.datetime.now().isoformat()),
+            ['id']
+        )
 
 
 def main():
-    show_project_list()
-    show_default_options()
-    process_user_input()
+    while True:
+        refresh()
 
 
 if __name__ == "__main__":
