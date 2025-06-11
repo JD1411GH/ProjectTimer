51bba81eb48ab7e95dece46a990723c501055290
Jayanta <jayanta.dn@gmail.com>
2025-06-11 19:29:52 +0530

    display running timers

--- src/cli.py
+++ src/cli.py
@@ -42,7 +42,47 @@ def show_default_options():
     print("0. New task")
     print("\n[s] Stop timer")
     print("[d] Daily report")
+    print("[t] Task report")
     print("[x] Exit")
+
+
+def show_project_list():
+    global table
+
+    # Project list
+    projects = [{"project": row['project']}
+                for row in table.distinct('project')]
+
+    # effort spent
+    today = datetime.date.today()
+    today_records = [
+        row for row in table.all()
+        if datetime.datetime.fromisoformat(row['start_time']).date() == today
+    ]
+    for record in today_records:
+        idx = next(
+            (i for i, p in enumerate(projects) if p['project'] == record['project']), None)
+        start_time = record['start_time']
+        end_time = record['end_time']
+        if end_time is None:
+            end_time = datetime.datetime.now().isoformat()
+            projects[idx]['timer_running'] = True
+        duration = datetime.datetime.fromisoformat(
+            end_time) - datetime.datetime.fromisoformat(start_time)
+        if 'duration' in projects[idx]:
+            projects[idx]['duration'] = projects[idx]['duration'] + duration
+        else:
+            projects[idx]['duration'] = duration
+
+    # display
+    print("\n")
+    for idx, proj in enumerate(projects, 1):
+        duration = proj.get('duration', datetime.timedelta())
+        hours_decimal = duration.total_seconds() / 3600
+        running = ""
+        if proj['timer_running']:
+            running = "*"
+        print(f"{idx}. {proj['project']} - {hours_decimal:.2f}{running} hrs")
 
 
 def start_timer(project, task):
@@ -60,6 +100,7 @@ def stop_timer():
 
 
 def main():
+    show_project_list()
     show_default_options()
     process_user_input()
 
